8f55d91e586f58e3cb2d676d8de2bcb8edb5f365
Muhammed Ali OZKAYA <m_aliozkaya@hotmail.com>
2021-10-17 22:08:15 +0300

    Certificate Task 1/4

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,31 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repository = new GenericRepository<Category>();
+
+        public List<Category> GetAllBL()
+        {
+            return repository.List();
+        }
+
+        public void AddCategoryBL(Category category)
+        {
+            if (category.Name == "" || category.Name.Length <= 3)
+            {
+                // Hata mesajı
+            } else
+            {
+                repository.Insert(category);
+            }
+        }
+    }
+}
