deec04c9b282245d6b7a3e5fcaf026843bc7a496
Muhammed Ali OZKAYA <m_aliozkaya@hotmail.com>
2021-10-19 12:41:42 +0300

    Added dynamic CategoryList

--- DotNetMvc/Controllers/CategoryController.cs
+++ DotNetMvc/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,18 @@ namespace DotNetMvc.Controllers
 {
     public class CategoryController : Controller
     {
+        CategoryManager categoryManager = new CategoryManager();
         // GET: Category
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetCategoryList()
+        {
+            var categoryValues = categoryManager.GetAllBL();
+
+            return View(categoryValues);
         }
     }
 }
