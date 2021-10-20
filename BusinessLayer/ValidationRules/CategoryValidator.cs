425e146d3f865c853ad0ecfcbd96d6689262e855
Muhammed Ali OZKAYA <m_aliozkaya@hotmail.com>
2021-10-20 23:45:53 +0300

    Fluent validation imported and category validator added

---
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidator : AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Kategori Adını Boş Geçemezsiniz");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklamayı Boş Geçemezsiniz");
+            RuleFor(x => x.Name).MinimumLength(3).WithMessage("Lütfen en az 3 karakter girişi yapın");
+            RuleFor(x => x.Name).MaximumLength(20).WithMessage("Lütfen 20 karakter üstünde değer girmeyin");
+
+        }
+    }
+}
