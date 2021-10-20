eb795629c1c577e95f1c6af719179f7b870042a5
Muhammed Ali OZKAYA <m_aliozkaya@hotmail.com>
2021-10-21 00:55:38 +0300

    Category add and list on adminCategory

---
+++ DotNetMvc/Controllers/AdminCategoryController.cs
@@ -0,0 +1,51 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetMvc.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var categories = cm.GetList();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            CategoryValidator validator = new CategoryValidator();
+            ValidationResult results = validator.Validate(category);
+
+            if (results.IsValid)
+            {
+                cm.Add(category);
+                return RedirectToAction("Index");
+            }
+
+            foreach (var item in results.Errors)
+            {
+                ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+            }
+
+            return View(category);
+
+        }
+    }
+}
