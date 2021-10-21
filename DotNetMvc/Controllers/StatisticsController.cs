63d0b7a9c0245e59f910d5806c3b2a2348dd654a
Muhammed Ali OZKAYA <m_aliozkaya@hotmail.com>
2021-10-21 21:28:38 +0300

    Statistics controller for statistics of headings, writers, categories.

---
+++ DotNetMvc/Controllers/StatisticsController.cs
@@ -0,0 +1,37 @@
+﻿using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetMvc.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        Context context = new Context();
+        // GET: Statistics
+        public ActionResult Index()
+        {
+            var categoryCount = context.Categories.Count();
+            ViewBag.categoryCount = categoryCount;
+
+            var softwareCategoryCount = context.Headings.Count(h => h.Category.Name == "yazılım");
+            ViewBag.categoryYazilimCount = softwareCategoryCount;
+
+            var writersCount = context.Writers.Count(w => w.Name.Contains("a"));
+            ViewBag.writersCount = writersCount;
+
+            var maxTitlesCategoryId = context.Headings.Max(x => x.Category.Id);
+            var categoryName = context.Categories.Find(maxTitlesCategoryId).Name;
+            ViewBag.maxTitlesCategory = categoryName;
+
+            var activeCategories = context.Categories.Count(c => c.Status == true);
+            var closeCategories = context.Categories.Count(c => c.Status == false);
+
+            ViewBag.gapActiveDeactiveCategories = Math.Abs(activeCategories - closeCategories);
+
+            return View();
+        }
+    }
+}
