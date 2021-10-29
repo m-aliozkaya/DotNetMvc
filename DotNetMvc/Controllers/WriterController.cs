f9d93a39f95e5b0dfc71f844b65e2ec94628ce5b
Muhammed Ali OZKAYA <m_aliozkaya@hotmail.com>
2021-10-30 01:34:42 +0300

    Writer controller and index view added

---
+++ DotNetMvc/Controllers/WriterController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetMvc.Controllers
+{
+    public class WriterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterDal());
+        // GET: Writer
+        public ActionResult Index()
+        {
+            var writerList = wm.GetList();
+            return View(writerList);
+        }
+    }
+}
