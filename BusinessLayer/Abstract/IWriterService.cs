29ccda5eb614a255b3b5cc174d0847fcb5ec94d9
Muhammed Ali OZKAYA <m_aliozkaya@hotmail.com>
2021-10-30 01:25:53 +0300

    Writer business operations

---
+++ BusinessLayer/Abstract/IWriterService.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IWriterService
+    {
+        List<Writer> GetList();
+        Writer GetWriterById(int id);
+        void AddWriter(Writer writer);
+        void DeleteWriter(Writer writer);
+        void UpdateWriter(Writer writer);
+
+    }
+}
