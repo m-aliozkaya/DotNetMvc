6efa8d51aec11c9672987fa4f21cf4ebf96083db
Muhammed Ali OZKAYA <m_aliozkaya@hotmail.com>
2021-10-31 14:23:02 +0300

    Add heading with dropdownlist for categories and writers

---
+++ BusinessLayer/Concrete/HeadingManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class HeadingManager : IHeadingService
+
+    {
+        public IHeadingDal _headingDal;
+
+        public HeadingManager(IHeadingDal headingDal)
+        {
+            _headingDal = headingDal;
+        }
+
+        public void Add(Heading heading)
+        {
+            _headingDal.Insert(heading);
+        }
+
+        public void Delete(Heading heading)
+        {
+            _headingDal.Delete(heading);
+        }
+
+        public Heading GetHeadingById(int id)
+        {
+            return _headingDal.Get(h => h.Id == id);
+        }
+
+        public List<Heading> GetList()
+        {
+            return _headingDal.List();
+        }
+
+        public void Update(Heading heading)
+        {
+            _headingDal.Update(heading);
+        }
+    }
+}
---
+++ DotNetMvc/Controllers/HeadingController.cs
@@ -0,0 +1,60 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetMvc.Controllers
+{
+    public class HeadingController : Controller
+    {
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        WriterManager wm = new WriterManager(new EfWriterDal());
+        // GET: Heading
+        public ActionResult Index()
+        {
+            var headings = hm.GetList();
+            return View(headings);
+        }
+
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            ViewBag.CategoryId = new SelectList(cm.GetList(), "Id", "Name");
+            ViewBag.WriterId = new SelectList(wm.GetList(), "Id", "Name");
+            return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult AddHeading(Heading heading)
+        {
+            heading.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            ViewBag.CategoryId = new SelectList(cm.GetList(), "Id", "Name");
+            ViewBag.WriterId = new SelectList(wm.GetList(), "Id", "Name");
+
+            HeadingValidator validator = new HeadingValidator();
+            ValidationResult result = validator.Validate(heading);
+
+            if (result.IsValid)
+            {
+
+                hm.Add(heading);
+                return RedirectToAction("Index");
+            }
+
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+            }
+
+            return View(heading);
+        }
+    }
+}
