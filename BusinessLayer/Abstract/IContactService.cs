c62caa90821c10d2d4ddac5b7caa028ffa9f186c
Muhammed Ali OZKAYA <m_aliozkaya@hotmail.com>
2021-11-16 18:12:27 +0300

    Contact business operations

---
+++ BusinessLayer/Abstract/IContactService.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IContactService
+    {
+        List<Contact> GetList();
+
+        void Add(Contact contact);
+
+        void Update(Contact contact);
+
+        void Delete(Contact contact);
+        Contact GetContactById(int id);
+    }
+}
---
+++ BusinessLayer/ValidationRules/ContactValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class ContactValidator : AbstractValidator<Contact>
+    {
+        public ContactValidator()
+        {
+            RuleFor(x => x.UserEmail).NotEmpty().WithMessage("Mail adresini boş geçemezsiniz.");
+            RuleFor(x => x.Subject).NotEmpty().WithMessage("Konu Adını boş geçemezsiniz.");
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Kullanıcı Adını boş geçemezsiniz.");
+            RuleFor(x => x.Subject).MinimumLength(3).WithMessage("Lütfen en az 3 karakter girişi yapınız.");
+            RuleFor(x => x.Message).MaximumLength(100).WithMessage("En fazla 100 karakter girebilirsiniz.");
+        }
+    }
+}
