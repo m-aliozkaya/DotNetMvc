d42a414e4ac7be92ffc5d248fb3c4ce55d2a657b
Muhammed Ali OZKAYA <m_aliozkaya@hotmail.com>
2021-11-17 17:11:56 +0300

    Message DataAccess and Business operations. and created simple message inbox page.

---
+++ BusinessLayer/Abstract/IMessageService.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IMessageService
+    {
+        List<Message> GetList();
+
+        void Add(Message message);
+
+        void Update(Message message);
+
+        void Delete(Message message);
+
+        Message GetCategoryById(int id);
+    }
+}
---
+++ DotNetMvc/Controllers/MessageController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetMvc.Controllers
+{
+    public class MessageController : Controller
+    {
+        MessageManager mm = new MessageManager(new EfMessageDal());
+        // GET: Message
+        public ActionResult Inbox()
+        {
+            var messageList = mm.GetList();
+            return View(messageList);
+        }
+    }
+}
