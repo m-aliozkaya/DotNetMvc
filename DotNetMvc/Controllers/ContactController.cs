cf55230b6422be8d42132c4c1e2a08aea29591ed
Muhammed Ali OZKAYA <m_aliozkaya@hotmail.com>
2021-11-17 14:19:33 +0300

    Contact message detail page

--- DotNetMvc/Controllers/ContactController.cs
+++ DotNetMvc/Controllers/ContactController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,19 @@ namespace DotNetMvc.Controllers
 {
     public class ContactController : Controller
     {
+        ContactManager cm = new ContactManager(new EfContactDal());
+        ContactValidator cv = new ContactValidator();
         // GET: Contact
         public ActionResult Index()
         {
-            return View();
+            var values = cm.GetList();
+            return View(values);
+        }
+
+        public ActionResult GetContactDetails(int id)
+        {
+            var contactDetail = cm.GetContactById(id);
+            return View(contactDetail);
         }
     }
 }
