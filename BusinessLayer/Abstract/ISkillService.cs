c9d889bd31ead006be9ed3f6ac40748eca03f731
Muhammed Ali OZKAYA <m_aliozkaya@hotmail.com>
2021-11-27 17:19:20 +0300

    Skill business and dataAccess finished.

---
+++ BusinessLayer/Abstract/ISkillService.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface ISkillService
+    {
+        List<Skill> GetSkills();
+
+        void Add(Skill skill);
+
+        void Update(Skill skill);
+
+        void Delete(Skill skill);
+        Skill GetSkillById(int id);
+    }
+}
---
+++ BusinessLayer/Concrete/SkillManager.cs
@@ -0,0 +1,46 @@
+﻿,using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class SkillManager : ISkillService
+    {
+        ISkillDal _skillDal;
+
+        public SkillManager(ISkillDal skillDal)
+        {
+            _skillDal = skillDal;
+        }
+
+        public void Add(Skill skill)
+        {
+            _skillDal.Insert(skill);
+        }
+
+        public void Delete(Skill skill)
+        {
+            _skillDal.Delete(skill);
+        }
+
+        public Skill GetSkillById(int id)
+        {
+            return _skillDal.Get(x => x.Id == id);
+        }
+
+        public List<Skill> GetSkills()
+        {
+            return _skillDal.List();
+        }
+
+        public void Update(Skill skill)
+        {
+            _skillDal.Update(skill);
+        }
+    }
+}
